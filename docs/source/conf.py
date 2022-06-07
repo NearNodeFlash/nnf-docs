04ec1e61ce1ee733160f230ab9cf38c604647b18
Bill Johnson <billj@hpe.com>
2022-06-07 17:05:21 -0500

    Updated doc build config
    Signed-off-by: Bill Johnson <billj@hpe.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Near Node Flash'
+copyright = '2022, Hewlett Packard Enterprise'
+author = ''
 
 release = '0.1'
 version = '0.1.0'
